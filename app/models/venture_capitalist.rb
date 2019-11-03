5a8181bcba8fa63baaf7a854b0fe1a5dd4ed9cf8
Aaron Klotnia <aklotnia@gmail.com>
2019-11-03 15:01:06 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,26 @@
-class VentureCapitalist
+require 'pry'
 
+class VentureCapitalist
+    attr_reader :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        rich_funding_rounds = FundingRound.all.select do |funding_round|
+            funding_round.venture_capitalist.total_worth > 1000000000
+        end
+        rich_funding_rounds.map do |rich_funding_round|
+            rich_funding_round.venture_capitalist
+        end
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+start1 = Startup.new("joe's startup", "joe", "facebook")
+start2 = Startup.new("jame's startup", "james", "udemy")
+start3 = Startup.new("jame's startup", "james", "udemy")
+venture1 = VentureCapitalist.new("joes dad", 10000000)
+venture2 = VentureCapitalist.new("james dad", 1000000001)
+venture3 = VentureCapitalist.new("jemima", 500000)
+funding1 = FundingRound.new(start1, venture1, "type A", 500000)
+funding2 = FundingRound.new(start2, venture2, "type B", 300000)
+funding3 = FundingRound.new(start2, venture1, "type A", 10000)
+funding3 = FundingRound.new(start2, venture3, "type A", 500)
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
