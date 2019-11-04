4dbd97c984beeab6b0da5ddf4601021bec79b8b7
eliskim27 <eliskim27@gmail.com>
2019-11-04 17:47:45 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,46 @@
 class VentureCapitalist
+    attr_reader :name, :total_worth
+    @@all = []
 
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select do |vc|
+            vc.total_worth > 1000000000
+        end
+    end
+###################
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |fr|
+            fr.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        funding_rounds.map do |fr|
+            fr.startup
+        end
+    end
+
+    def biggest_investment
+        funding_rounds.map do |fr|
+            fr.investment
+        end.max
+    end
+
+    def invested(domain)
+        
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,19 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+s1 = Startup.new("Co 1", "Steve", "Finance")
+s2 = Startup.new("Co 2", "Frank", "CPGs")
+s3 = Startup.new("Co 3", "Susan", "Tech")
+
+vc1 = VentureCapitalist.new("VC 1", 10000000000)
+vc2 = VentureCapitalist.new("VC 2", 40000000)
+vc3 = VentureCapitalist.new("VC 3", 8000000)
+
+fr1 = FundingRound.new(s1, vc3, "Angel", 50000)
+fr2 = FundingRound.new(s2, vc1, "Series D", 999999)
+fr3 = FundingRound.new(s3, vc2, "Series A", 200000)
+
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+"endofpry"
