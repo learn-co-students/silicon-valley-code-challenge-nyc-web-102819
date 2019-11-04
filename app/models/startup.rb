1e6e99981d5519cd0c510b450d081d3a1832a94d
lukewaring <lukewaring@gmail.com>
2019-11-04 16:01:07 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,65 @@
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
 
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def funding_rounds 
+        FundingRound.all.select do |round|
+            round.startup == self
+        end
+    end
+
+    def num_funding_rounds
+        funding_rounds.length
+    end
+
+    def total_funds
+        funding_rounds.reduce(0) do |sum, round|
+            sum + round.investment
+        end
+    end
+
+    def investors
+        funding_rounds.collect do |round|
+            round.venture_capitalist
+        end.uniq
+    end
+
+    def big_investors
+        investors.select do |investor|
+            investor.total_worth > 1000000000.00    
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        all.find do |startup|
+            startup.founder == founder
+        end
+    end
+
+    def self.domains
+        all.collect do |startup|
+            startup.domain                    
+        end
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+techcorp = Startup.new("Techcorp", "Alice Apple", "fintech")
+medcorp = Startup.new("Medcorp", "Bill Bruce", "medtech")
+legalcorp = Startup.new("Legalcorp", "Alice Apple", "legaltech")
+
+barney = VentureCapitalist.new("Barney Bourne", 1000000000.99)
+marissa = VentureCapitalist.new("Marissa Mayer", 5000000000.99)
+bill = VentureCapitalist.new("Bill Gurley", 100000000.99)
+
+round1 = FundingRound.new(techcorp, barney, "Series A", 5000000.00)
+round2 = FundingRound.new(techcorp, bill, "Series B", 10000000.00)
+round2_5 = FundingRound.new(techcorp, bill, "Series C", 20000000.00)
+round3 = FundingRound.new(medcorp, barney, "Series B", 15000000.00)
+round4 = FundingRound.new(medcorp, barney, "Series C", 25000000.00)
+
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+#leave this here to ensure binding.pry isn't the last line
